689b449df88a66c8d56688d8e32857c4cc198843
darshan <darsh2605@gmail.com>
2023-03-30 00:33:07 +0530

    Chapter 6, All  shloka is added properly

--- lib/Screens/SholkaScreen.dart
+++ lib/Screens/SholkaScreen.dart
@@ -569,7 +569,7 @@ class _SholkaScreenState extends State<SholkaScreen> {
                     color: kmarroncolor,
                     child:  ListView.builder(
                       shrinkWrap: true,
-                      itemCount: 48,
+                      itemCount: 47,
                       itemBuilder: (context, i) {
                         return ListTile(
                           title: Padding(
@@ -582,6 +582,7 @@ class _SholkaScreenState extends State<SholkaScreen> {
                               child: OutlinedButton(
                                 onPressed: () =>
                                 {
+                                  Navigator.push(context, PageTransition(type: PageTransitionType.fade, child: ShlokaDetailScreen(listIndex : i,chapter: 6)))
 
                                 },
                                 style: ButtonStyle(
