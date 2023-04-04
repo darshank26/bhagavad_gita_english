df792beaf3a2574bc8790d96b79ee99739af77db
darshan <darsh2605@gmail.com>
2023-04-05 00:25:25 +0530

    Chapter 9, 8 shloka is added properly

---
+++ lib/utils/chapter9.dart
@@ -0,0 +1,22 @@
+List<String> chapter_nine_trans_sanskrit = [
+  "Shri Bhagavaan uvaacha. Idam tu te guhyatamam pravakshyaamyanasooyave. Jnaanam vijnaana sahitam yajjnaatvaa mokshyase'shubhaat.",
+  "Rajavidya rajaguhyam pavitramidamuttamam. Pratyakshavagamam dharmyam susukham kartumavyayam.",
+  "Ashraddhadhaanaah purushaa dharmasyaasya parantapa, Apraapya maam nivartante mrityusamsaaravartmani.",
+  "Maya tatamidam sarvam jagadavyaktamoortinaa, Matsthaani sarvabhootani na chaaham teshvavasthitah.",
+  "Na cha matsthaani bhootani pashya me yogamaishwaram, Bhootabhrin na cha bhootastho mamatmaa bhootabhaavana.",
+  "Yathaa kaashasthito nityam vaayuh sarvatrago mahan, Tathaa sarvaani bhutaani matsthaaneetyupadhaaraya.",
+  "Sarva-bhūtāni kaunteya prakrtim yānti māmikām, kalpa-ksaye punas tāni kalpādau visrjāmy aham.",
+  "Prakritim svam avastabhya visrijami punah punah, bhuta-gramamamam krtsnam avasam prakriter vasat.",
+  ];
+
+
+List<String> chapter_nine_english = [
+  "\"Shri Bhagavan said, 'I shall now reveal to you this most secretive knowledge and wisdom, knowing which you shall be liberated from the miseries of material existence. This knowledge is combined with realized experience, and by understanding it, you shall be freed from the cycle of birth and death.'\"",
+  "This knowledge is the king of education, the most secret of all secrets. It is the purest knowledge, and because it gives direct perception of the self by realization, it is the perfection of religion. It is everlasting, and it is joyfully performed.",
+  "Those who are not faithful in this devotional service cannot attain Me, O conqueror of enemies. Therefore they return to the path of birth and death in this material world.",
+  "By Me, in My unmanifested form, this entire universe is pervaded. All beings are in Me, but I am not in them.",
+  "And yet everything that is created does not rest in Me. Behold My mystic opulence! Although I am the maintainer of all living entities, and although I am everywhere, still My Self is the very source of creation.",
+  "As the mighty wind, blowing everywhere, rests always in the sky, in the same way, all beings rest in Me.",
+  "O son of Kunti, all created beings enter into My nature at the end of a cycle of time; and I create them again at the beginning of the next cycle.",
+  "By Me, in My unmanifested form, this entire universe is pervaded. All beings are in Me, but I am not in them.",
+  ];
