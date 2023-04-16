45c30e7656af4dca13ee4ef68aea93ea48a92d1e
darshan <darsh2605@gmail.com>
2023-04-16 16:05:31 +0530

    Chapter 12,  6 shloka is added properly

---
+++ lib/utils/chapter12.dart
@@ -0,0 +1,23 @@
+List<String> chapter_12_trans_sanskrit = [
+  "Arjuna uvācha: evaṁ satatayuktā ye bhaktāstvāṁ paryupāsate, ye cāpyakṣaramavyaktaṁ teṣāṁ ke yogavittamāḥ",
+  "śrī bhagavān uvācha: mayyāveśya mano ye māṁ nityayuktā upāsate, śraddhayā parayopetāḥ te me yuktatamā matāḥ",
+  "ye tavakṣaramanirdeśyamavyaktaṁ paryupāsate, sarvatra-gamachintyaṁ cha kūṭasthamachalandhruvam",
+  "sanniyamyendriyagrāmaṁ sarvatra samabuddhayaḥ, te prāpnuvanti māmeva sarvabhūtahite ratāḥ",
+  "kleśo'dhikatarasteṣām avyaktāsakta-cetasām, avyaktā hi gatirduḥkhaṁ dehavadbhiravāpyate",
+  "Ye tu sarvāṇi karmāṇi mayi sannyasya matparāḥ ananyenaiva yogena māṁ dhyāyanta upāsate",
+  "Teshaamaham samuddhartaa mrityusamsaarasagarat Bhavaami nachiraat paartha mayyaaveshitachetasam",
+  "Mayyeva mana aadhatsva mayi buddhim niveshaya Nivasishyasi mayyeva ata uurdhvam na samshayah",
+];
+
+
+
+List<String> chapter_12_english = [
+"Arjuna said: Those devotees who, ever steadfast, worship You with devotion by always speaking of You and meditating upon You, and those who worship the imperishable and the unmanifested—that which is beyond all conception—what is the nature of their devotion?",
+  "The Supreme Lord said:Those who fix their minds on Me and always engage in My devotion with steadfast faith, I consider them to be the most perfect in yoga.",
+  "But those who worship the imperishable, the indefinable, the unmanifest, the omnipresent, the unthinkable, the unchanging, the immovable and the eternal, they undoubtedly attain Me.",
+  "With their senses under control and with a mind that is equal everywhere, such devotees, engaged in the welfare of all beings, surely attain Me.",
+  "The sufferings of those who are attached to the unmanifested are greater, for one's attachment to the unmanifested is difficult to overcome. The embodied form is temporary and is the cause of all pain, but the unmanifested is eternal and therefore beyond pain.",
+  "But those who dedicate all their actions to Me, regarding Me as the Supreme goal, meditating on Me with single-minded devotion and worshiping Me with exclusive love, I swiftly deliver them from the ocean of birth and death, for their consciousness is united with Me.",
+  "For those who have their minds set on Me, I am the swift deliverer from the ocean of birth and death. O Arjuna, for such steadfast devotees of Mine, whose thoughts have merged with Me, I become the swift deliverer from the ocean of birth and death.",
+  "Fix your mind on Me alone, engage your intellect in Me; thus you shall surely abide in Me alone hereafter, without a doubt.",
+];
