8db88d8e3c31cb26fa6c8461452905faf6732636
darshan <darsh2605@gmail.com>
2023-04-18 09:00:38 +0530

    Chapter 13,  16 shloka is added properly

--- lib/utils/chapter13.dart
+++ lib/utils/chapter13.dart
@@ -7,7 +7,15 @@ List<String> chapter_13_trans_sanskrit = [
   "Mahābhūtānyahaṅkāro buddhiravyaktameva cha Indriyāṇi daśaikaṁ cha pañca chendriyagocarāḥ",
   "icchā dvesaḥ sukham duḥkham saṅghātaścetanā dhrtiḥ, etat ksetram samāsena savikāram udāhrtam",
   "amānitvam adambhitvam ahimsā ksāntir ārjavam ācāryopāsanam śaucaṁ sthairyam ātma-vinigrahaḥ",
-  ];
+  "Indriyartheshu vairagyam anahankara eva cha Janmamrityujaravyadhidukhadoshanudarshanam",
+  "Asaktiranabhishvangah putradaragrihadeshhu Nityam cha samachittatvam ishtanishtopapattishu",
+  "Mayi cha ananyayogena bhaktiravyabhicharini Viviktadeshasevitvam aratirjanasamsadi",
+  "Adhyatma-jnananityatvam tattva-jnanartha-darshanam Etaj-jnanamiti proktam ajnanam yato'nyatha",
+  "jñeyaḿ yattatpravakṣyāmi yajjñātvāmṛtam aśnute anādimat-paraḿ brahma na sat tan nāsad ucyate",
+  "Sarvatah pāṇipādam tat sarvato 'kṣiśiromukham sarvatah śrutimal lōkē sarvam āvṛtya tiṣṭhati",
+  "sarvendriya-gunabhasam sarvendriya-vivarjitam asaktam sarvabhricchaiva nirgunam guna-bhoktr ca",
+  "Bahirantashcha bhutānāmacharam charam eva cha Sūkshmatvāttadavigñeyam dūrasthaṁ chāntike cha tat",
+];
 
 
 List<String> chapter_13_english = [
@@ -19,4 +27,12 @@ List<String> chapter_13_english = [
   "the five senses and the mind, along with the ego and intellect, Are the eightfold division of My material energy, O Arjuna.",
   "the desire for pleasure and aversion to pain, the union of consciousness and willpower, and perseverance, these constitute the field and its modifications.",
   "Humility, modesty, non-violence, forgiveness, honesty, service to the teacher, cleanliness, steadfastness, and self-control.",
-  ];
+  "One should develop detachment towards sensual pleasures and ego. By observing the problems of birth, death, old age, disease, and suffering, one should cultivate detachment. This verse emphasizes the importance of attaining mental peace and happiness, which is only possible by letting go of ego and developing detachment towards material pleasures.",
+  "One should not be attached to children, wealth, home, and other possessions. One should always maintain equanimity of mind in all situations and treat the agreeable and disagreeable with equal detachment. This verse emphasizes that the source of happiness in human life is not wealth, children, or home, but rather it is achieved through a healthy mental state and maintaining equanimity in the face of life's problems.",
+  "Devotion to me should be undivided and steadfast. One should live in a secluded place and be indifferent to the objects of the senses while carrying out their duties in the midst of people.",
+  "Spiritual knowledge is eternal and the vision of truth is the aim of that knowledge. This is called knowledge, and anything else is ignorance.",
+  "That which is to be known, I will declare to you, knowing which, one attains immortality. The supreme Brahman is without beginning. That non-existent (asat) never existed, nor will it ever exist.",
+  "Everywhere are Its hands and feet, Its eyes, heads, and faces. Its ears are in all places, and it pervades everything in the universe.",
+  "Appearing as the qualities of all the senses, yet devoid of all the senses, detached and all-pervading, without attributes and the enjoyer of attributes.",
+  "The living beings, both moving and non-moving, are within and without, and they are also both subtle and gross. This truth is indeed difficult to understand, being both far away and very near.",
+];
