567b55377125025e946f876714274e56fc1e7631
darshan <darsh2605@gmail.com>
2023-04-29 21:46:25 +0530

    Chapter 14, 8 sholka is added

--- lib/utils/chapter14.dart
+++ lib/utils/chapter14.dart
@@ -1,8 +1,22 @@
 List<String> chapter_14_trans_sanskrit = [
-
-  ];
+  "Shri Bhagavaan Uvaacha, Param bhuyaḥ pravakshyaami jñaanaanaam jñaanamuttamam, Yajjnaatvaa munayah sarve paraam siddhim ito gataah.",
+  "Idam jnanam upashritya mama sadharmyam agatah, Sarge'pi nopajayante pralaye na vyathanti cha.",
+  "Mama yonirmahad brahma tasmim garbham dadhamy aham, Sambhavah sarva-bhutanam tato bhavati bharata.",
+  "Sarva-yonishu kaunteya murtyah sambhavanti yah, Tasam brahma mahad yonir aham bija-pradah pita.",
+  "Sattvam rajas tama iti gunah prakriti-sambhavah, Nibadhnanti mahabaho dehe dehinam avyayam.",
+  "Tatra sattvam nirmalatvaat prakaashakam anaamayam, Sukha-sangen badhnaati jnaana-sangen chaanagha.",
+  "Rajo raagaatmakaṁ viddhi tṛṣṇaa-saṅga-samudbhavam, Tannibadhnāti kaunteya karma-saṅgena dehinam.",
+  "Tamastvajñānajaṁ viddhi mohanam sarva-dehinām, Pramād-ālasya-nidrābhis tannibadhnāti bhārata.",
+];
 
 
 List<String> chapter_14_english = [
-
+  "I shall now declare unto thee again that supreme wisdom, the best of all knowledge, knowing which all the sages have attained to supreme perfection after this life.",
+  "Taking refuge in this knowledge of Mine, those who have attained My nature are not reborn even during creation, nor do they suffer during dissolution.",
+  "My womb is the great Brahman (the ultimate reality), and in that I place the germ (of all beings). From that, O Arjuna, comes the birth of all beings.",
+  "Whatever forms are born from all the different kinds of wombs, O Arjuna, the great Brahman is their womb and I am the seed-giving father.",
+  "The three qualities - sattva (goodness), rajas (passion), and tamas (ignorance) - born of nature, O mighty-armed Arjuna, bind fast the immutable embodied soul in the body.",
+  "Of these, the mode of goodness, which is pure, illuminating and free from disease, binds the soul by creating attachment to happiness and knowledge, O sinless one.",
+  "Know that the mode of passion is characterized by desire and attachment to fruitive work, O Arjuna. It binds the embodied soul to material objects by means of attachment to the results of activities.",
+  "O Arjuna, know that ignorance-born tamas produces delusion for all embodied beings. It binds them with the ropes of negligence, indolence and sleep.",
   ];
