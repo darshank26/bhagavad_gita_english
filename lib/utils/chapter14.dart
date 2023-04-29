fc8ddde7b011340b8dcfd34544d644a7a314e6a9
darshan <darsh2605@gmail.com>
2023-04-29 21:54:04 +0530

    Chapter 14, 16 sholka is added

--- lib/utils/chapter14.dart
+++ lib/utils/chapter14.dart
@@ -7,6 +7,15 @@ List<String> chapter_14_trans_sanskrit = [
   "Tatra sattvam nirmalatvaat prakaashakam anaamayam, Sukha-sangen badhnaati jnaana-sangen chaanagha.",
   "Rajo raagaatmakaṁ viddhi tṛṣṇaa-saṅga-samudbhavam, Tannibadhnāti kaunteya karma-saṅgena dehinam.",
   "Tamastvajñānajaṁ viddhi mohanam sarva-dehinām, Pramād-ālasya-nidrābhis tannibadhnāti bhārata.",
+  "sattvaṃ sukhe sañjayati rajaḥ karmaṇi bhārata jñānam āvṛtya tu tamaḥ pramāde sañjayaty uta",
+  "Rajas tamashchaabhibhooya sattvam bhavati bhaarata, Rajah sattvam tamashchaiva tamah sattvam rajas tathaa.",
+  "Sarva-dvāreṣu dehe 'smin prakāśa upajāyate, jñānaṁ yadā tadā vidyād vivṛddhaṁ sattvam ityuta.",
+  "lobhaḥ pravṛttirārambhaḥ karmaṇāmaśamaḥ spṛhā, rajasyetāni jāyante vivṛddhe bharatarṣabha",
+  "Aprakāśo'pravṛttiśca pramādo moha eva ca Tamasyetāni jāyante vivṛddhe kurunandana",
+  "Yada sattve pravriddhe tu pralayam yati dehabhrit, tadottamavidaam lokan amalan pratipadyate.",
+  "Rajasi pralayam gatva karmānāmashayam jagat Tathāpralīna-tamasi mūḍha-yoniṣhu jāyate",
+  "karmaṇaḥ su-kṛtasyāhuḥ sāttvikam nir-malaṁ phalam rajasas tu phalaṁ duḥkham ajñānaṁ tamasaḥ phalam",
+
 ];
 
 
@@ -19,4 +28,12 @@ List<String> chapter_14_english = [
   "Of these, the mode of goodness, which is pure, illuminating and free from disease, binds the soul by creating attachment to happiness and knowledge, O sinless one.",
   "Know that the mode of passion is characterized by desire and attachment to fruitive work, O Arjuna. It binds the embodied soul to material objects by means of attachment to the results of activities.",
   "O Arjuna, know that ignorance-born tamas produces delusion for all embodied beings. It binds them with the ropes of negligence, indolence and sleep.",
+  "O Arjuna, sattva binds one to happiness, rajas to action, while tamas, covering knowledge, binds one to delusion in negligence.",
+  "O Arjuna, by predominance of rajas, tamas, or sattva, the qualities of those modes are generated. The nature of sattva arises from predominance over rajas and tamas, rajas arises from predominance over sattva and tamas, and tamas arises from predominance over sattva and rajas.",
+  "When the light of knowledge illuminates all the gates of the body, then it is said that the person has attained a state of purity and goodness (sattva).",
+  "Desire, activity, and restlessness arise when Rajas is dominant, O Arjuna. These qualities cause a person to be attached to action, result in excessive striving, and are the source of craving.",
+  "Increased tamas (darkness) gives rise to qualities such as lack of illumination, inactivity, negligence, and delusion, O Arjuna!",
+  "When the embodied being passes beyond the three modes of material nature (goodness, passion, and ignorance) which are the source of the body, he attains to the pure transcendental knowledge and reaches the stage of supreme peace, in which the soul attains everlasting happiness.",
+  "When one perishes in Rajas, the cause of activity, the world of the active is born; in the same way, when one perishes in Tamas, the deluded darkness is born in the world.",
+  "They say that the fruit of good action is pure and consists of sattva, while the fruit of action born of rajas is pain and the fruit of action born of tamas is ignorance.",
   ];
