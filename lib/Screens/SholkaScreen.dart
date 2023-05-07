fad098299849ea26ed6503ca9c518d98e8da2a6b
darshan <darsh2605@gmail.com>
2023-05-07 15:29:17 +0530

    Chapter 16, all sholkas is added

--- lib/Screens/SholkaScreen.dart
+++ lib/Screens/SholkaScreen.dart
@@ -1457,7 +1457,7 @@ class _SholkaScreenState extends State<SholkaScreen> {
                     color: kmarroncolor,
                     child:  ListView.builder(
                       shrinkWrap: true,
-                      itemCount: 25,
+                      itemCount: 24,
                       itemBuilder: (context, i) {
                         return ListTile(
                           title: Padding(
@@ -1470,6 +1470,7 @@ class _SholkaScreenState extends State<SholkaScreen> {
                               child: OutlinedButton(
                                 onPressed: () =>
                                 {
+                                  Navigator.push(context, PageTransition(type: PageTransitionType.fade, child: ShlokaDetailScreen(listIndex : i,chapter: 16)))
 
                                 },
                                 style: ButtonStyle(
