c4d8b3d261e8b1531c49f5f462fa5cb1584a5f78
darshan <darsh2605@gmail.com>
2023-05-09 23:02:00 +0530

    Chapter 18, 8 sholkas is added

---
+++ lib/utils/chapter18.dart
@@ -0,0 +1,21 @@
+List<String> chapter_18_trans_sanskrit = [
+  "Arjuna uvācha: saṁnyāsasya mahā-bāho tattvam icchāmi veditum tyāgasya ca hṛṣīkeśa pṛthak keśiṇiśūdana",
+  "Shrī Bhagavān uvācha: kāmyānāṁ karmaṇāṁ nyāsaṁ sannyāsaṁ kavayo viduḥ sarva-karma-phala-tyāgaṁ prāhus tyāgaṁ vichakṣaṇāh",
+  "tyājyaṁ doṣa-vad ity eke karma prāhur manīṣiṇaḥ yajña-dāna-tapaḥ-karma na tyājyam iti cāpare",
+  "nishchayaṁ śhṛiṇu me tatra tyāge bharata-sattama tyāgo hi puruṣha-vyāghra trividhaḥ samprakīrtitaḥ",
+  "yajñadāna-tapaḥ-karma na tyājyaṁ kāryam eva tat yajño dānaṁ tapaś chaiva pāvanāni manīṣhiṇām",
+  "etānyapi tu karmāṇi saṅgaṁ tyaktvā phalāni cha kartavyānīti me pārtha niśchitaṁ matam uttamam",
+  "niyatasya tu sannyāsaḥ karmaṇo nopapadyate mohāt tasya parityāgas tāmasaḥ parikīrtitaḥ",
+  "duḥkham ityeva yat karma kāya-kleśa-bhayāt tyajet sa kṛtvā rājasaṁ tyāgaṁ naiva tyāga-phalaṁ labhet",
+  ];
+
+List<String> chapter_18_english = [
+"Arjuna said: O mighty-armed one, I wish to understand the principles of renunciation and the nature of the renounced order of life. O Hrishikesha, please explain these to me separately.",
+  "The Supreme Lord said: The wise know that giving up actions that are motivated by desire is renunciation, and they call the giving up of the fruits of all actions the renunciation of the results of action.",
+  "Some learned men declare that all kinds of work should be given up as faulty, while others say that acts of sacrifice, charity, and penance should not be abandoned.",
+  "O best of the Bharatas, now please listen to me and my conclusive words on the matter of renunciation. Renunciation of material desires is certainly the highest renunciation, and it is of threefold nature, as I will now explain.",
+  "Acts of sacrifice, charity, and penance should never be abandoned; they must be performed. Indeed, sacrifice, charity, and penance purify even the great souls.",
+  "Furthermore, O Arjuna, it is my supreme and conclusive opinion that all actions should be performed without attachment to the results. This is my final and best instruction to you.",
+  "However, giving up actions completely out of delusion is not appropriate for one who is seeking perfection. Such renunciation is said to be in the mode of ignorance."
+  "One should give up actions that are motivated by pain alone, and also those that cause physical strain and fear. Renunciation performed in this way is said to be in the mode of passion and is not productive of any real benefit.",
+  ];
